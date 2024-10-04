14e8c288e141de1aaa401755fc4f6df191b31e4b
HaticeSudenas <haticesudenas@gmail.com>
2024-10-04 18:48:26 +0300

    init

--- src/layouts/main/content/collection/index.js
+++ src/layouts/main/content/collection/index.js
@@ -4,7 +4,7 @@ import { ChevronDownIcon } from '@heroicons/react/outline';
 export default function Collection() {
 
     return (
-        <div className=" md:w-full border-2 border-black-400 ml-4 mb-20  w-full h-[500px] bg-white shadow-2xl rounded-2xl p-4 mt-24">
+        <div className=" md:w-full border-2 border-black-400  mb-20  w-full h-[500px] bg-white shadow-2xl rounded-2xl p-4 mt-24">
             <div className="text-3xl font-bold text-center">Digiturk Eğlencenin ve Avrupanın Yıldızı Sıkça Sorulan Sorular</div>
             <div className=" p-4">
                 <Disclosure>
